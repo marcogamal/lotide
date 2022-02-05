0a01d409ac3bc765c6ce104520193193f4e8f309
Marco <marco.salman@gmail.com>
2022-02-05 09:07:28 -0500

    findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(title,name) {
+  let genreArray = Object.keys(title)
+  for (let element of genreArray) {
+    if (title[element] === name) {
+      console.log(title[element])
+      console.log(element)
+      console.log(genreArray)
+      return element
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
