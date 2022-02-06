b2471f57de4177405988f3e103e98fe26c9d606f
Marco <marco.salman@gmail.com>
2022-02-06 17:15:01 -0500

    takeUntil

---
+++ takeUntil.js
@@ -0,0 +1,34 @@
+const takeUntil = function (array, callback) {
+  let data = [];
+  for (let element of array) {
+    if (callback(element) === false) {
+      data.push(element);
+    } else {
+      return data;
+    }
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, (x) => x < 0);
+console.log(results1);
+
+console.log("---");
+
+const data2 = [
+  "I've",
+  "been",
+  "to",
+  "Hollywood",
+  ",",
+  "I've",
+  "been",
+  "to",
+  "Redwood",
+];
+const results2 = takeUntil(data2, (x) => x === ",");
+console.log(results2);
+
+// [ 1, 2, 5, 7, 2 ]
+// ---
+// [ 'I\'ve', 'been', 'to', 'Hollywood' ]
