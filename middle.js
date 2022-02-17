c4d5c3ad2093d3925dfc64a803b0747e0014a046
Marco <marco.salman@gmail.com>
2022-02-16 21:21:06 -0500

    middle refactoring

--- middle.js
+++ middle.js
@@ -1,31 +1,11 @@
-const assertArraysEqual = function (actual, expected) {
-  if (
-    actual[0] === expected[0] &&
-    actual[1] === expected[1] &&
-    actual[2] === expected[2]
-  ) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function (a, b) {
-  if (a[0] === b[0] && a[1] === b[1] && a[2] === b[2]) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
 function middle(arr) {
   if (arr.length < 3) {
-    return [];
+    return []; // return empty array for array with 1 or 2 element
   } else {
-    let removeBeginning = arr.splice(0, arr.length / 2 + 1);
+    let removeBeginning = arr.splice(0, arr.length / 2 + 1); 
     let removeEnding = removeBeginning.splice(0, arr.length);
-    return removeBeginning;
+    return removeBeginning; // return 1 element if odd, and 2 elements if even
   }
 }
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3, 4, 5, 6]))
+
+module.exports = middle
---
+++ test/middleTest.js
@@ -0,0 +1,4 @@
+const middle = require("../middle")
+
+console.log(middle([1, 2]));
+console.log(middle([1, 2, 3, 4, 5, 6]))
