661e65a8a214d5f1fe6241f9788aad0672d28d3d
Marco <marco.salman@gmail.com>
2022-02-17 20:33:32 -0500

    refactoring

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,51 +1,18 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-const eqArrays = function (a, b) {
-  if (a[0] === b[0] && a[1] === b[1] && a[2] === b[2]) {
-    return true;
-  } else {
-    return false;
-  }
-};
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
-const eqObjects = function (object1, object2) {
-  let key1 = Object.keys(object1);
-  let key2 = Object.keys(object2);
-  if (key1.length !== key2.length) {
-    return false;
-  }
-  for (let element of key1) {
-    if (Array.isArray(object1[element]) && Array.isArray(object2[element])) {
-      return eqArrays(object1[element], object2[element]);
-    } else {
-      if (object1[element] !== object2[element]) {
-        return false;
-      }
-    }
-  //   console.log(element);
-  //   console.log(object1[element]);
-  //   console.log(object2[element]);
-  }
-  return true;
-};
-
-const assertObjectsEqual = function(actual, expected) {
-  const inspect = require('util').inspect;
+const assertObjectsEqual = function (actual, expected) {
+  const inspect = require("util").inspect;
   if (
     actual[0] === expected[0] &&
     actual[1] === expected[1] &&
     actual[2] === expected[2]
   ) {
-    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+    console.log(
+      `✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`
+    );
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+    console.log(
+      `🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`
+    );
   }
 };
 
-assertObjectsEqual({1:2}, {1:2});
+module.exports = assertObjectsEqual;
---
+++ test/assertObjectsEqualTest.js
@@ -0,0 +1,3 @@
+assertObjectsEqual = require("../assertObjectsEqual");
+
+assertObjectsEqual({ 1: 2 }, { 1: 2 });
