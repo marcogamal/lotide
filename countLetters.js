dd4100b08b7829797584b59009307ef78fb98ab5
Marco <marco.salman@gmail.com>
2022-02-17 20:38:06 -0500

    refactoring

--- countLetters.js
+++ countLetters.js
@@ -1,15 +1,7 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 // have to loop
 // result is an object
 // space not counted therefore deleted
 // count each letter
-
 function countLetters(str) {
   let count = {};
   const trimmedStr = str.split(' ').join('')
@@ -24,18 +16,5 @@ function countLetters(str) {
   }
   return count;
 }
-console.log(countLetters("lighthouse in the house"))
 
-// would return 
-// {
-//   l: 1,
-//   i: 2,
-//   g: 1,
-//   h: 4,
-//   t: 2,
-//   o: 2,
-//   u: 2,
-//   s: 2,
-//   e: 3,
-//   n: 1,
-// }
+module.exports = countLetters
---
+++ test/countLettersTest.js
@@ -0,0 +1,17 @@
+countLetters = require('../countLetters')
+
+console.log(countLetters("lighthouse in the house"))
+
+// would return 
+// {
+//   l: 1,
+//   i: 2,
+//   g: 1,
+//   h: 4,
+//   t: 2,
+//   o: 2,
+//   u: 2,
+//   s: 2,
+//   e: 3,
+//   n: 1,
+// }
